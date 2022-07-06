e2543e0ae6b99c126ab9ad19b01cb81e0c0f0e4e
Hasan Yahya <hasanyah@gmail.com>
2022-07-06 17:48:11 +0200

    Add new entry modal

--- app-frontend/src/components/JobPost.vue
+++ app-frontend/src/components/JobPost.vue
@@ -24,28 +24,66 @@ getJobPosts()
 </script>
 
 <template>
-<div class="container">
-    <h1 class="text-center">Job Posts</h1>
-    
-    <div v-show="loading" class="spinner-border spinner-border-sm" role="status">
-        <span class="sr-only">Loading...</span>
+    <div class="container">
+        <h1 class="text-center">Job Posts</h1>
+
+        <div v-show="loading" class="spinner-border spinner-border-sm" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+
+
+        <table v-show="!loading" class="table table-striped">
+            <thead>
+                <th>Title</th>
+                <th>Company</th>
+                <th>Applied</th>
+                <th>Status</th>
+                <th>Location</th>
+                <th>URL</th>
+            </thead>
+            <tbody>
+                <CustomDoubleRow v-for="jp in jobposts" :key="jp.id" :data="jp" />
+            </tbody>
+        </table>
+
+        <button v-show="!loading" type="button" class="btn btn-primary btn-lg" data-bs-toggle="modal"
+            data-bs-target="#addJobPostModal">Add new entry</button>
+        <!-- Modal -->
+        <div class="modal fade" id="addJobPostModal" tabindex="-1" aria-labelledby="addJobPostModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addJobPostModalLabel">Add New Job Post</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="mb-3">
+                                <label for="Job title" class="col-form-label">Job title:</label>
+                                <input type="text" class="form-control" id="job-title">
+                            </div>
+                            <div class="mb-3">
+                                <label for="message-text" class="col-form-label">Company Name:</label>
+                                <input type="text" class="form-control" id="company-name">
+                            </div>
+                            <div class="mb-3">
+                                <label for="message-text" class="col-form-label">Location:</label>
+                                <input type="text" class="form-control" id="company-location">
+                            </div>
+                            <div class="mb-3">
+                                <label for="message-text" class="col-form-label">URL:</label>
+                                <input type="text" class="form-control" id="job-url">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- TODO: Map integration would be cool -->
     </div>
-
-    
-    <table class="table table-striped">
-        <thead>
-            <th>Title</th>
-            <th>Company</th>
-            <th>Applied</th>
-            <th>Status</th>
-            <th>Location</th>
-            <th>URL</th>
-        </thead>
-        <tbody>
-            <CustomDoubleRow v-for="jp in jobposts" :key="jp.id" :data="jp"/>
-        </tbody>
-    </table>
-
-    <!-- TODO: Map integration would be cool -->
-</div>
 </template>
